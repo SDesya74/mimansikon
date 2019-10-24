1fcab7e5b2676a20f02206469ade7c5bc62736c5
Саня Череп <SDesya74@gmail.com>
2019-10-24 23:45:21 +0300

    Added

---
+++ Entities/Dices/DiceQueue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Mimansikon.Entities.Dices {
+	public class DiceQueue<T> {
+		public List<(T, List<Dice>)> Queue { get; private set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
