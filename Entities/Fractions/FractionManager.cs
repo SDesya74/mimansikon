c44e5e9fe3c00bddfd361d5a974c79364108fbd5
Саня Череп <SDesya74@gmail.com>
2019-10-25 23:18:44 +0300

    Added Save & Load methods

--- Entities/Fractions/FractionManager.cs
+++ Entities/Fractions/FractionManager.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Mimansikon.Misc;
 
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+using System.Collections.Generic;
 
 namespace Mimansikon.Entities.Fractions {
 	public static class FractionManager {
 		public static List<Fraction> List;
 		
 		public static void Init() {
+			List = new List<Fraction>();
+		}
 
+		public static void Save() {
+			DataSaver.Save("fractions", List);
+		}
+
+		public static void Load() {
+			List = (List<Fraction>) DataSaver.Read("fractions") ?? new List<Fraction>();
 		}
 
 	}
