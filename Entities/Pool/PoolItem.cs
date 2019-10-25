88eb31cda640249d0fd8ae32d4c044d8077dde85
Саня Череп <SDesya74@gmail.com>
2019-10-25 23:48:05 +0300

    Path changed

--- Entities/Pool/PoolItem.cs
+++ Entities/Pool/PoolItem.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+﻿using Mimansikon.Entities.Items;
 
 namespace Mimansikon.Entities.Pool {
 	class PoolItem {
+		public ItemType Type { get; set; }
+		public int Count { get; set; } = 0;
+		public ItemParameters Parameters { get; set; }
 	}
 }
