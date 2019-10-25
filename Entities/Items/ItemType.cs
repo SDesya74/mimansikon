a670237495d72ee252bf99ddf25276b867744f5a
Саня Череп <SDesya74@gmail.com>
2019-10-25 23:48:25 +0300

    Changed path

---
+++ Entities/Items/ItemType.cs
@@ -0,0 +1,28 @@
+﻿namespace Mimansikon.Entities.Items {
+	public enum ItemType {
+		Giant,
+		Club,
+
+		Horse,
+		UpgradedHorse,
+		ArmoredHorse,
+
+		LongWeapon,
+		SmallWeapon,
+		MagicWeapon,
+		Spear,
+
+		Bow,
+		Quiver,
+
+		Cuirass,
+		Helmet,
+
+		BigShield,
+		SmallShield,
+
+		Unit,
+
+		Gun
+	}
+}
