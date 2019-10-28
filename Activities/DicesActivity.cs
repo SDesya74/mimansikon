786c43383b2fdf1b2cc9efa71add5da0a143c2d2
Саня Череп <SDesya74@gmail.com>
2019-10-28 23:04:53 +0300

    Added

---
+++ Activities/DicesActivity.cs
@@ -0,0 +1,15 @@
+﻿
+using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace Mimansikon.Activities {
+	[Activity(Label = "@string/dices_activity")]
+	public class DicesActivity : AppCompatActivity {
+		protected override void OnCreate(Bundle savedInstanceState) {
+			base.OnCreate(savedInstanceState);
+
+			// Create your application here
+		}
+	}
+}
