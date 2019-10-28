8c2012cd90af9297ca52ddfd5b44dc89624e0ba4
Саня Череп <SDesya74@gmail.com>
2019-10-28 23:03:31 +0300

    Added

---
+++ ViewUtil/Button.cs
@@ -0,0 +1,29 @@
+﻿using Android.Content;
+using Android.Graphics;
+using Android.Runtime;
+
+using Mimansikon.Util;
+
+using System;
+
+namespace Mimansikon.ViewUtil {
+	class Button : Android.Widget.Button {
+		public Button(Context context) : base(context) {
+			SetTypeface(FontManager.Current, Android.Graphics.TypefaceStyle.Bold);
+		}
+
+		private Android.Graphics.Color _color;
+		public Android.Graphics.Color Color {
+			get {
+				return _color;
+			}
+			set {
+				_color = value;
+				Background.SetColorFilter(value, PorterDuff.Mode.Multiply);
+			}
+		}
+
+		protected Button(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer) {
+		}
+	}
+}
