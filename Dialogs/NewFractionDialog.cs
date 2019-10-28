7603f50fbaba3594631b27f64a884b59e9e2669b
Саня Череп <SDesya74@gmail.com>
2019-10-28 23:04:04 +0300

    Added

---
+++ Dialogs/NewFractionDialog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Android.Animation;
+using Android.Content;
+using Android.Graphics;
+using Android.Views;
+using Android.Widget;
+using Mimansikon.Util;
+using Mimansikon.ViewUtil;
+using TextView = Mimansikon.ViewUtil.TextView;
+
+namespace Mimansikon.Dialogs {
+	class NewFractionDialog {
+		public NewFractionDialog(Context context) {
+
+		}
+	}
+}
