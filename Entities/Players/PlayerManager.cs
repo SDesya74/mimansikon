e52f4b7ea76702ae8e6c72e38d7faf428f618f3a
Саня Череп <SDesya74@gmail.com>
2019-10-28 23:03:54 +0300

    Added

---
+++ Entities/Players/PlayerManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Mimansikon.Util;
+
+namespace Mimansikon.Entities.Players {
+	class PlayerManager {
+		public static List<Player> List;
+
+		public static void Init() {
+			List = new List<Player>();
+		}
+
+		public static void Save() {
+			DataSaver.Save("players", List);
+		}
+
+		public static void Load() {
+			List = (List<Player>) DataSaver.Read("players") ?? new List<Player>();
+		}
+	}
+}
