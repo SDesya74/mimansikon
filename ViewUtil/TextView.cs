b336e1cf041c6a8cec2105cdf7995f8ea1b9e774
Саня Череп <SDesya74@gmail.com>
2019-10-28 23:03:19 +0300

    Added

---
+++ ViewUtil/TextView.cs
@@ -0,0 +1,23 @@
+﻿using Android.Content;
+using Android.Graphics;
+using Android.Runtime;
+
+using Mimansikon.Util;
+
+using System;
+
+namespace Mimansikon.ViewUtil {
+	class TextView : Android.Widget.TextView {
+		public TextView(Context context) : base(context) {
+			Typeface = FontManager.Current;
+		}
+
+		protected TextView(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer) {
+			
+		}
+
+		public void SetTypefaceStyle(TypefaceStyle style) {
+			SetTypeface(FontManager.Current, style);
+		}
+	}
+}
