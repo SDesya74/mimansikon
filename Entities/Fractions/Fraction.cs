92100d038af6c3f3f874d6af26a883793a3372bf
Саня Череп <SDesya74@gmail.com>
2019-11-06 16:57:12 +0300

    Some changes

--- Entities/Fractions/Fraction.cs
+++ Entities/Fractions/Fraction.cs
@@ -3,6 +3,10 @@
 namespace Mimansikon.Entities {
 	[Serializable]
 	public class Fraction {
+		public Fraction(string name) {
+			this.Name = name;
+		}
+
 		public String Name { get; set; }
 		public String Description { get; set; }
 	}
