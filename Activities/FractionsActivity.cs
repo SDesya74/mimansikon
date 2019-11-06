3c35beafe12010a258b2a94c4d679b1def3ba8c9
Саня Череп <SDesya74@gmail.com>
2019-11-06 16:56:50 +0300

    Some changes

--- Activities/FractionsActivity.cs
+++ Activities/FractionsActivity.cs
@@ -17,49 +17,49 @@ using static Android.Views.ViewGroup.LayoutParams;
 using ActionBar = Mimansikon.ViewUtil.ActionBar;
 using Activity = Android.App.Activity;
 using TextView = Mimansikon.ViewUtil.TextView;
+using Button = Mimansikon.ViewUtil.Button;
+using System;
 
 namespace Mimansikon.Activities {
 	[Activity(Label = "@string/fractions_activity")]
 	public class FractionsActivity : ViewUtil.Activity {
 		LinearLayout Main;
 		protected override ActionBar OnCreateActionBar(ActionBar bar) {
-			bar.Color = new Color(GetColor(Resource.Color.colorMimansikonPurple));
+			bar.Color = new Color(GetColor(Resource.Color.colorPrimaryDark));
 			bar.TextColor = Color.White;
-			bar.Title = GetString(Resource.String.app_name);
+			bar.Title = GetString(Resource.String.fractions_activity);
+
+			bar.AddButton(GetDrawable(Resource.Drawable.add), view => {
+				var dialog = new NewFractionDialog(this);
+				dialog.SetSize(Screen.Width / 4 * 3, Screen.Height / 2);
+				dialog.AddButton("Ok", v => {
+					if(String.IsNullOrWhiteSpace(dialog.FractionName)) return false;
+					Fraction fraction = new Fraction(dialog.FractionName);
+					FractionManager.Add(fraction);
+					BuildList();
+					return true;
+				});
+				dialog.ShowAt(view);
+
+			});
 			return bar;
 		}
 
 		protected override void OnCreateContent(ViewGroup Content) {
-			using(ObservableScrollView scroll = new ObservableScrollView(this)) {
-				//SetContentView(scroll);
+			var scroll = new ObservableScrollView(this);
+			scroll.LayoutParameters = new RelativeLayout.LayoutParams(MatchParent, MatchParent);
+			Content.AddView(scroll);
 
-				Main = new LinearLayout(this);
-				scroll.AddView(Main);
-			}
-
-			var parameters = new RelativeLayout.LayoutParams(WrapContent, WrapContent);
-			parameters.AddRule(LayoutRules.AlignParentEnd);
-			parameters.AddRule(LayoutRules.AlignParentBottom);
-			parameters.BottomMargin = 30.Dip();
-			parameters.MarginEnd = 30.Dip();
-
-			FloatingActionButton button = new FloatingActionButton(this) {
-				LayoutParameters = parameters
+			Main = new LinearLayout(this) {
+				Orientation = Orientation.Vertical
 			};
-			button.SetImageDrawable(GetDrawable(Resource.Drawable.add));
-			button.ColorNormal = GetColor(Resource.Color.colorPrimary);
-			button.ColorPressed = GetColor(Resource.Color.colorPrimaryDark);
-			button.Click += delegate {
-				var dialog = new NewFractionDialog(this);
-				dialog.SetSize(Screen.Width / 4 * 3, Screen.Height / 2);
-				dialog.ShowAt(button);
-			};
-			AddContentView(button);
+			Main.LayoutParameters = new RelativeLayout.LayoutParams(MatchParent, MatchParent);
+			scroll.AddView(Main);
+
 			BuildList();
 		}
 
 		public void BuildList() {
-			FractionManager.Load();
 			Main.RemoveAllViews();
 			FractionManager.List.ForEach(e => AddItem(e));
 		}
@@ -67,19 +67,34 @@ namespace Mimansikon.Activities {
 		public void AddItem(Fraction item) {
 			LinearLayout main = new LinearLayout(this);
 			main.SetGravity(GravityFlags.Center | GravityFlags.Left);
-			main.SetPadding(3.Dip(), 3.Dip(), 3.Dip(), 3.Dip());
+			main.SetPadding(5.Dip(), 15.Dip(), 5.Dip(), 15.Dip());
+			main.Click += delegate {
+				OpenFractionInfoActivity(item);
+			};
 			Main.AddView(main);
 
 			TextView name = new TextView(this) {
 				Text = item.Name,
 				Gravity = GravityFlags.Center | GravityFlags.Left
 			};
-			name.SetTypeface(Typeface.Default, TypefaceStyle.Bold);
+			name.SetTypefaceStyle(TypefaceStyle.Bold);
 			main.AddView(name);
 		}
 
-		protected override void OnActivityResult(int requestCode, Result resultCode, Intent data) {
-			if(requestCode == 740) BuildList();
+
+
+		private static int InfoRequestCode = 740;
+		private void OpenFractionInfoActivity(Fraction fraction) {
+			Intent intent = new Intent(this, typeof(FractionInfoActivity));
+			StartActivityForResult(intent, InfoRequestCode);
+		}
+
+
+		protected override void OnActivityResult(int requestCode, Result result, Intent intent) {
+			if(requestCode == InfoRequestCode) {
+				FractionManager.Load();
+				BuildList();
+			}
 		}
 	}
 }
