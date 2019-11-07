cc7ff02e6c640881a6cc86e443c93318bd67e714
Саня Череп <SDesya74@gmail.com>
2019-11-08 00:25:25 +0300

    Optimized usings

--- Entities/Dices/DiceQueue.cs
+++ Entities/Dices/DiceQueue.cs
@@ -4,12 +4,10 @@ namespace Mimansikon.Entities.Dices {
 	public class DiceQueue<T> {
 		public List<(T, List<Dice>)> Queue { get; private set; }
 
-
 		public DiceQueue(List<T> list) {
 			Queue = new List<(T, List<Dice>)>();
 			list.ForEach(e => Queue.Add((e, new List<Dice>())));
 		}
-
 
 		public void Randomize() {
 		}
